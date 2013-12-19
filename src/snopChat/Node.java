eb8094e4e8aca261cffc2f0ab314fc0b27e4b898
kulizhsy <kulizhsy@tcd.ie>
2013-12-19 00:56:34 +0000

    problems committing,everything as before blah blah blah

--- src/snopChat/Node.java
+++ src/snopChat/Node.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.UnknownHostException;
 
 /**class that should be sending and receiving things**/
 public class Node {
